e2451b97f31bcb7a9ee71ed36c590b69bbefbcc4
Don Carpenter <doca@dhigroup.com>
2022-01-27 23:49:24 +1000

    Cosmetic

--- src/ui/routes/config/router.ts
+++ src/ui/routes/config/router.ts
@@ -12,6 +12,7 @@ states.forEach((state) => router.stateRegistry.register(state));
 router.urlService.rules.initial({ state: 'home' });
 
 import reqAuthHook from '@/ui/routes/config/requiredAuthHook';
+
 router.transitionService.onBefore(reqAuthHook.criteria, reqAuthHook.callback, { priority: 10 });
 
 export { router };
